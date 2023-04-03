c6bd1502b7a26b9f06335888914e50638399f2f7
guinafelix <oguilhermeleitefelix@gmail.com>
2023-04-03 17:57:23 -0300

    feat: add answers

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
 import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
+import { NewAccount } from './class/newAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
 console.log(peopleAccount)
-peopleAccount.deposit()
+peopleAccount.deposit(12)
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
+companyAccount.deposit(34)
 console.log(companyAccount)
+
+const newAccount: NewAccount = new NewAccount('teste', 30)
+
+newAccount.deposit(100)
+console.log(newAccount.getBalance())
--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,5 +1,5 @@
 export abstract class DioAccount {
-  private name: string
+  private readonly name: string
   private readonly accountNumber: number
   balance: number = 0
   private status: boolean = true
@@ -9,30 +9,29 @@ export abstract class DioAccount {
     this.accountNumber = accountNumber
   }
 
-  setName = (name: string): void => {
-    this.name = name
-    console.log('Nome alterado com sucesso!')
-  }
-
   getName = (): string => {
     return this.name
   }
 
-  deposit = (): void => {
+  deposit = (value: number): void => {
     if(this.validateStatus()){
-      console.log('Voce depositou')
+      this.balance += value
     }
   }
 
-  withdraw = (): void => {
-    console.log('Voce sacou')
+  withdraw = (value: number): void => {
+    if (this.validateStatus() && this.balance >= value) {
+      this.balance -= value
+      return
+    }
+    console.log('Saldo insuficiente ou conta inválida')
   }
 
   getBalance = (): void => {
     console.log(this.balance)
   }
 
-  private validateStatus = (): boolean => {
+  protected validateStatus = (): boolean => {
     if (this.status) {
       return this.status
     }
